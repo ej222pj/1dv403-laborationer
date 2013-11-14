a058a4029d0cf8834e70e87d1319f9259ea5e1e8
Eric Sjöström Jennerstrand <ej222pj@student.lnu.se>
2013-11-14 12:42:45 -0500

    Kommit på hur man ändrar små till stora

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -7,10 +7,21 @@ window.onload = function(){
 		// Plats för förändring.		
 		// Returnera den konverterade strängen.
 		// Vid fel, kasta ett undantag med ett meddelande till användaren. 
-	
+		var newStr = "";
+	for(var i = 0; i < str.length; i++)
+	{
+        if(str[i] == str[i].toLowerCase())
+        {
+            newStr += str[i].toUpperCase();
+        }
+        else if(str[i] == str[i].toUpperCase())
+        {
+            newStr += str[i].toLowerCase();
+        }
+	}
 
 
-
+    return newStr;
 
 
 
