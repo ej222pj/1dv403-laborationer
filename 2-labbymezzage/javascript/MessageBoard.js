0a8a8765a53a5ee76e5e234af2310c4ed2fd883a
Eric Sjöström Jennerstrand <ej222pj@student.lnu.se>
2013-11-27 20:04:34 -0500

    klar med steg 5, tror det funkar

--- 2-labbymezzage/javascript/MessageBoard.js
+++ 2-labbymezzage/javascript/MessageBoard.js
@@ -1,5 +1,8 @@
 "use strict";
 (function () {
+    var submit = document.getElementById("submit"),
+    input = document.querySelector("#readMessage");
+    
     var MessageBoard = {
         
         messages: [],
@@ -11,5 +14,17 @@
         }
         
     };
-     window.onload = MessageBoard.init("dasd");
+     //window.onload = MessageBoard.init("dasd");
+     
+    //Denna functionen lyssnar på om man klickar på skicka knappen
+    submit.addEventListener("onclick", function (e){
+        e = e || event; //Event är för IE
+        e.preventDefault();
+        if((input.value).trim().length === 0){
+            return;
+        }
+        MessageBoard.createMessage(input.value.trim());
+        MessageBoard.renderMessages();
+    }, false);
+    
 }());
