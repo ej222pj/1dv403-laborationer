05138fbe2e40d3e64a435c2cc0b6bd6a8035de14
Eric Sjöström Jennerstrand <ej222pj@student.lnu.se>
2013-12-16 18:40:04 -0500

    börjat med uppgift 4

---
+++ 4-forms/script/Validator.js
@@ -0,0 +1,9 @@
+"use strict";
+window.onload = function(){
+    var form = document.getElementById("form");
+    form.onsubmit = function(e){
+        var fn = form.elements["Förnamn"];
+        
+        //Returnera false om allt inte går igenom!      
+    };
+};
