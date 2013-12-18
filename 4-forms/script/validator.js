47049ebbd0d7a2e9e8a5edfb726e98affccbcd8a
Eric Sjöström Jennerstrand <ej222pj@student.lnu.se>
2013-12-18 14:02:02 -0500

    Klar, måste bara styla lite

--- 4-forms/script/validator.js
+++ 4-forms/script/validator.js
@@ -2,9 +2,10 @@
 (function () {
     var form = document.getElementById("form");
 
-    // Creates an error message for the given input node.
+    // Skapar ett error meddelande om man trycker på skicka
     var addError = function (node, text) {
         var label = document.getElementById(node.getAttribute("name")),
+        //var label = form.elements[node].getAttribute("name"),
             explanation = document.createElement("small"),
             explanationText = document.createTextNode(text);
 
@@ -16,7 +17,7 @@
         node.parentNode.insertBefore(explanation, node.nextSibling);
     };
 
-    // If the node has an error message, this method removes it.
+    // plockar bort error meddelandet om de inte behövs mer
     var removeError = function (node) {
         var label = document.getElementById(node.getAttribute("name")),
             explanation;
@@ -31,24 +32,23 @@
         }
     };
 
-    // The control-functions returns true if the user input is valid.
+    // Function som kör testet emot rätt input, returnerar true om de går igenom
     var controlInput = function (regex, input) {
         return regex.test(input);
     };
 
-    // Returns true if all inputs are valid.
+    // Kollar alla inputs returnerar true om allt är som det ska
     var controlInputs = function () {
-        var allValid = true,
-            i;
+        var allValid = true, i;
 
-        for (i = 0; i <= form.elements.length - 3; i += 1) {
-            // Control name fields.
+        for (i = 1; i <= form.elements.length - 3; i += 1) {
+            // Kolla namnfälten
             if (form.elements[i].getAttribute("class").indexOf("name") !== -1) {
                 if (!controlInput(/^.{1,255}$/, form.elements[i].value)) {
                     allValid = false;
-                    addError(form.elements[i], "Fältet får ej lämnas blankt.");
+                    addError(form.elements[i], "Fältet får inte lämnas blankt.");
                 }
-            // Control postal code fields.
+            // Kolla postkodfältet
             } else if (form.elements[i].getAttribute("class").indexOf("postalCode") !== -1) {
                 if (!controlInput(/^(SE)?[\ ]?[\d]{3}(-|\ )?[\d]{2}$/, form.elements[i].value)) {
                     allValid = false;
@@ -56,7 +56,7 @@
                 } else {
                     form.elements[i].value = form.elements[i].value.replace(/(SE|\ |-)/g, "");
                 }
-            // Control email fields.
+            // Kolla email fältet
             } else if (form.elements[i].getAttribute("class").indexOf("email") !== -1) {
                 if (!controlInput(/^\w+([\.\+]?\w+)*@([\w]+)\.([a-zA-Z]{2,6})$/, form.elements[i].value)) {
                     allValid = false;
@@ -67,8 +67,7 @@
         return allValid;
     };
     
-    // Creates and adds a modal popup with the data from the form elements.
-    var createModalPopup = function () {
+    var createPopup = function () {
         var div0 = document.createElement("div"),
             div1 = document.createElement("div"),
             div2 = document.createElement("div"),
@@ -77,44 +76,51 @@
             headerText = document.createTextNode("Vänligen bekräfta ditt köp"),
             div4 = document.createElement("div"),
             div5 = document.createElement("div"),
-            button1 = document.createElement("button"),
-            button2 = document.createElement("button"),
-            button1Text = document.createTextNode("Bekräfta köp"),
-            button2Text = document.createTextNode("Avbryt");
+            accButton = document.createElement("button"),
+            removeButton = document.createElement("button"),
+            accButtonText = document.createTextNode("Bekräfta köp"),
+            removeButtonText = document.createTextNode("Avbryt");
 
         div0.setAttribute("id", "overlay");
-        div1.setAttribute("id", "modalDiv");
+        div1.setAttribute("id", "popUp");
 
-        // Reads the data from the form elements.
-        for (var i = 0; i <= form.elements.length - 2; i += 1) {
+        // Läser data från formuläret till popupen
+        for (var i = 1; i <= form.elements.length - 2; i += 1) {
             var p = document.createElement("p");
-            var text = document.createTextNode(form.elements[i].value);
+            p.setAttribute("class", "popupInfo");
+            var text = document.createTextNode(document.getElementById(form.elements[i].name).textContent);
             p.appendChild(text);
             div4.appendChild(p);
+            p = document.createElement("p");
+            p.setAttribute("class", "popUptext");
+            text = document.createTextNode(form.elements[i].value);
+            p.appendChild(text);
+            div4.appendChild(p);
+            
         }
 
-        button1.setAttribute("type", "button");
-        button2.setAttribute("type", "button");
+        accButton.setAttribute("type", "button");
+        removeButton.setAttribute("type", "button");
 
-        // This is the only way to submit the form...
-        button1.addEventListener("click", function (e) {
-            removeModalPopup();
+        // Subimt formuläret
+        accButton.addEventListener("click", function (e) {
+            removePopup();
             setDisabledAllFields(false);
             form.submit();
             form.reset();
         }, false);
-        button2.addEventListener("click", function (e) {
-            removeModalPopup();
+        removeButton.addEventListener("click", function (e) {
+            removePopup();
             setDisabledAllFields(false);
         }, false);
 
         h2.appendChild(headerText);
         header.appendChild(h2);
         div2.appendChild(header);
-        button1.appendChild(button1Text);
-        button2.appendChild(button2Text);
-        div5.appendChild(button2);
-        div5.appendChild(button1);
+        accButton.appendChild(accButtonText);
+        removeButton.appendChild(removeButtonText);
+        div5.appendChild(removeButton);
+        div5.appendChild(accButton);
         div1.appendChild(div2);
         div1.appendChild(div4);
         div1.appendChild(div5);
@@ -122,25 +128,24 @@
         document.body.appendChild(div1);
     };
 
-    // Removes the modal Popup.
-    var removeModalPopup = function () {
+    // Ta bort popup
+    var removePopup = function () {
         var div0 = document.getElementById("overlay"),
-            div1 = document.getElementById("modalDiv");
-
+            div1 = document.getElementById("popUp");
+        
         document.body.removeChild(div0);
         document.body.removeChild(div1);
     };
 
-    // Disables and undisables all the form elements.
+    // gör formuläret grått
     var setDisabledAllFields = function (bool) {
-        for (var i = 0; i <= form.elements.length - 1; i += 1) {
+        for (var i = 1; i <= form.elements.length - 1; i += 1) {
             form.elements[i].disabled = bool;
         }
     };
 
     form.addEventListener("submit", function (e) {
         e = e || window.event;
-        // The real submit-button is in the modal popup...
         e.preventDefault();
 
         removeError(form.elements[1]);
@@ -155,7 +160,7 @@
 
         if (controlInputs()) {
             setDisabledAllFields(true);
-            createModalPopup();
+            createPopup();
         }
         else{
             
