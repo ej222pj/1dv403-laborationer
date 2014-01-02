16caa19da4f72e4a848df40f19f3e64b72d1bf26
Eric Sjöström Jennerstrand <ej222pj@student.lnu.se>
2014-01-02 13:41:35 -0500

    Ggjort ett ajax anrop, klar med andra sprintten

---
+++ 5-pwd/script/ImageLoader.js
@@ -0,0 +1,66 @@
+"Use strict";
+var ImageLoader = function () {
+    ImageLoader.prototype.getThumbPics();
+};
+
+// Ser till att ImageViewer ärver från superklassen Window
+ImageLoader.prototype = Object.create(Window.prototype);
+
+// Ajaxanrop som kommer att returnera JSON-sträng med tumnagelbilder 
+ImageLoader.prototype.getThumbPics = function () {
+    // Sätter timer på ajaxanropet, en animerad gif-bild visas om anropet 
+    //till servern drar ut på tiden
+    $(document).ready(function () {
+        setTimeout(function () { $('#ajaxloader').show(); }, 300);
+        $.ajax({
+            url: "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/"
+        }).done(function (data) {
+            $('#ajaxloader').hide();
+            var thumbs = $.parseJSON(data);
+            ImageLoader.prototype.renderThumbs(thumbs);
+        }).fail(function (jqXHR, textStatus) {
+            console.log("Läsfel, status: " + textStatus);
+        }); 
+    });
+};
+
+ImageLoader.prototype.renderThumbs = function (thumbs) {
+    var thumbDiv, thumb, a, i, contentDiv = document.querySelector(".galleryMain"), size = setSize(thumbs);
+    
+    for (i = 0; i < thumbs.length; ++i) {
+        // Skapar boxar till tumnaglarna    
+        thumbDiv = document.createElement("div");
+        thumbDiv.className = "thumbdiv";
+        thumbDiv.style.width = size.width + "px";
+        thumbDiv.style.height = size.height + "px";
+    
+        // Skapar tumnagelbilder
+        thumb = document.createElement("img");
+        a = document.createElement("a");
+        a.setAttribute("href", "#");
+        thumb.src = thumbs[i].thumbURL;
+    
+        a.appendChild(thumb);
+        thumbDiv.appendChild(a);
+        contentDiv.appendChild(thumbDiv);
+    } 
+    
+    // Tar fram tumnagelns bredd och höjd
+    function setSize(thumbs) {
+        var width = 0,
+            height = 0,
+            t;
+        
+        for (t in thumbs) {
+            if (thumbs[t].thumbWidth > width) {
+                width = thumbs[t].thumbWidth;
+            }
+            if (thumbs[t].thumbHeight > height) {
+                height = thumbs[t].thumbHeight;
+            }
+        } return {
+            width: width,
+            height: height
+        };
+    }
+};
---
+++ 5-pwd/script/imageWindow.js
@@ -0,0 +1,73 @@
+"use strict";
+(function () {
+    var continer = document.querySelector("#container");
+    var showGalleryImg = document.querySelector(".bottomMenu");
+    
+    var galleryLink = document.createElement("a");
+    var galleryImg = document.createElement("img");
+        
+    galleryLink.setAttribute("href", "#");
+    galleryImg.setAttribute("src", "pics/icon_sm_img.png");
+    galleryImg.setAttribute("class", "menuPicGallery");
+    
+    showGalleryImg.appendChild(galleryLink);
+    galleryLink.appendChild(galleryImg);
+    
+    var createGalleryPopup = function (){
+        var div0 = document.createElement("div"),
+        headerDiv = document.createElement("div"),
+        mainDiv = document.createElement("div"),
+        footerDiv = document.createElement("div"),
+        imgDiv = document.createElement("div"),
+        mainLink = document.createElement("a"),
+        mainImg = document.createElement("img"),
+        p = document.createElement("p"),
+        headerInfo = document.createTextNode("Gallery"),
+        close = document.createElement("div"),
+        closeLink = document.createElement("a");
+        
+        close.setAttribute("class", "closeGallery");
+        div0.setAttribute("class", "galleryContainer");
+        headerDiv.setAttribute("class", "galleryHeader");
+        mainDiv.setAttribute("class", "galleryMain");
+        footerDiv.setAttribute("class", "galleryFooter");
+        imgDiv.setAttribute("class", "headerInfo");
+        mainLink.setAttribute("href", "#");
+        mainImg.setAttribute("src", "pics/icon_sm_img.png");
+        mainImg.setAttribute("class", "galleryHeaderPic");
+        closeLink.setAttribute("href", "#");
+        close.innerHTML += "X";
+        
+        closeLink.appendChild(close);
+        p.appendChild(headerInfo);
+        mainLink.appendChild(mainImg);
+        imgDiv.appendChild(mainLink);
+        imgDiv.appendChild(p);
+        imgDiv.appendChild(closeLink);
+        headerDiv.appendChild(imgDiv);
+        div0.appendChild(headerDiv);
+        div0.appendChild(mainDiv);
+        div0.appendChild(footerDiv);
+        continer.appendChild(div0);
+        
+        closeLink.addEventListener("click", function(e){
+        e = e || window.event;
+        e.preventDefault();
+        
+        removeGalleryPopup();
+    }, false);
+    };
+    
+    var removeGalleryPopup = function (){
+        var div0 = document.querySelector(".galleryContainer");
+        
+        continer.removeChild(div0);
+    };
+    galleryLink.addEventListener("click", function(e){
+        e = e || window.event;
+        e.preventDefault();
+        
+        createGalleryPopup();
+        new ImageLoader();
+    }, false);
+}());
